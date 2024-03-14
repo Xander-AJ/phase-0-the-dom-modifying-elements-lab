756b9cd2a562046869bdad85de40dc403be84e23
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-14 15:09:59 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the <main> element with the id "main"
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element and set its id to "victory"
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Set the text content of the newHeader variable to "John-Alexander Kamau is the champion"
+newHeader.textContent = "John-Alexander Kamau is the champion";
+
+// Append the newHeader to the document body or any other desired location
+document.body.append(newHeader);
